94b83f8012cc088437ac4d1ce084dcccf47add0c
shalinth-adith <shalinth.adith@outlook.com>
2025-05-21 19:18:08 +0530

    code has been improved

--- guessing_game/ContentView.swift
+++ guessing_game/ContentView.swift
@@ -8,17 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia","France","Germany","Ireland","Italy","Nigeria","Poland","Spain","Ukraine","United Kingdom","US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30){
+                VStack{
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                ForEach(0..<3){ number in
+                    Button{
+                        flagTapped(number)
+                    }label: {
+                        Image(countries[number])
+                    }
+                    
+                }
+            }
         }
-        .padding()
+        .alert(scoreTitle,isPresented: $showingScore){
+            Button("Continue",action: askQuestion)
+        }message:{
+            Text("Your score is ??? ")
+        }
+    }
+    func flagTapped(_ number:Int){
+        if number == correctAnswer{
+            scoreTitle = "Correct"
+        }else{
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
 #Preview {
     ContentView()
 }
+
