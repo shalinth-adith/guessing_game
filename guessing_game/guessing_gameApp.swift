ef96dcccece07843e727338ee077174aea8a1326
shalinth-adith <shalinth.adith@outlook.com>
2025-05-21 19:13:18 +0530

    Initial Commit

---
+++ guessing_game/guessing_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessing_gameApp.swift
+//  guessing_game
+//
+//  Created by shalinth adithyan on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct guessing_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ guessing_gameTests/guessing_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  guessing_gameTests.swift
+//  guessing_gameTests
+//
+//  Created by shalinth adithyan on 21/05/25.
+//
+
+import Testing
+@testable import guessing_game
+
+struct guessing_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
